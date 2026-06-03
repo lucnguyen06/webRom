ad730e240d578272dcbf6ee459668e15cdd0af00
Lực Nguyễn - RIO <lucnguyen0562@gmail.com>
2026-06-04 00:21:11 +0700

    rio

--- script.js
+++ script.js
@@ -425,8 +425,8 @@ const lightboxCounter = document.getElementById('lightbox-counter');
 
 // Gallery configuration
 const galleryImages = {
-  os2: Array.from({ length: 7 }, (_, i) => `images_devices/os2/a${i + 1}.jpg`),
-  os3: Array.from({ length: 7 }, (_, i) => `images_devices/os3/a${i + 1}.jpg`)
+  os2: Array.from({ length: 6 }, (_, i) => `images_devices/os2/a${i + 1}.jpg`),
+  os3: Array.from({ length: 6 }, (_, i) => `images_devices/os3/a${i + 1}.jpg`)
 };
 
 let currentGallery = 'os3';
